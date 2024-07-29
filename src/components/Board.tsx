38db941964b395cbf52cc4b011afe85c27dd15e0
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2024-07-29 23:46:54 +0300

    Mergw add mock server

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,100 +1,99 @@
-import React, { useState, useEffect } from "react";
-import { DragDropContext } from "react-beautiful-dnd";
-import Column from "./Column";
+import React, {useState, useEffect} from 'react';
+import {DragDropContext} from 'react-beautiful-dnd';
+import Column from './Column';
 
 export default function Board() {
-    const [completed, setCompleted] = useState([]);
-    const [incomplete, setIncomplete] = useState([]);
-    const [backlog, setBacklog] = useState([]);
-    const [inReview, setInReview] = useState([]);
+  const [completed, setCompleted] = useState([]);
+  const [incomplete, setIncomplete] = useState([]);
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/todos")
-            .then((response) => response.json())
-            .then((json) => {
-                setCompleted(json.filter((task) => task.completed));
-                setIncomplete(json.filter((task) => !task.completed));
-            });
-    }, []);
+  console.log('%c ||||| incomplete', 'color:yellowgreen', incomplete);
 
-    const handleDragEnd = (result) => {
-        const { destination, source, draggableId } = result;
+  const [inReview, setInReview] = useState([]);
 
-        if (!destination || source.droppableId === destination.droppableId) return;
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => {
+        setCompleted(json.filter((task) => task.completed));
+        setIncomplete(json.filter((task) => !task.completed));
+      });
+  }, []);
 
-        deletePreviousState(source.droppableId, draggableId);
+  const handleDragEnd = (result) => {
+    const {destination, source, draggableId} = result;
 
-        const task = findItemById(draggableId, [...incomplete, ...completed, ...inReview, ...backlog]);
+    if (!destination || source.droppableId === destination.droppableId) return;
 
-        setNewState(destination.droppableId, task);
+    deletePreviousState(source.droppableId, draggableId);
 
-    };
+    const task = findItemById(draggableId, [
+      ...incomplete,
+      ...completed,
+      ...inReview,
+    ]);
 
-    function deletePreviousState(sourceDroppableId, taskId) {
-        switch (sourceDroppableId) {
-            case "1":
-                setIncomplete(removeItemById(taskId, incomplete));
-                break;
-            case "2":
-                setCompleted(removeItemById(taskId, completed));
-                break;
-            case "3":
-                setInReview(removeItemById(taskId, inReview));
-                break;
-            case "4":
-                setBacklog(removeItemById(taskId, backlog));
-                break;
-        }
+    setNewState(destination.droppableId, task);
+  };
 
+  function deletePreviousState(sourceDroppableId, taskId) {
+    switch (sourceDroppableId) {
+      case '1':
+        setIncomplete(removeItemById(taskId, incomplete));
+        break;
+      case '2':
+        setCompleted(removeItemById(taskId, completed));
+        break;
+      case '3':
+        setInReview(removeItemById(taskId, inReview));
+        break;
+      case '4':
+        setBacklog(removeItemById(taskId, backlog));
+        break;
     }
-    function setNewState(destinationDroppableId, task) {
-        let updatedTask;
-        switch (destinationDroppableId) {
-            case "1":   // TO DO
-                updatedTask = { ...task, completed: false };
-                setIncomplete([updatedTask, ...incomplete]);
-                break;
-            case "2":  // DONE
-                updatedTask = { ...task, completed: true };
-                setCompleted([updatedTask, ...completed]);
-                break;
-            case "3":  // IN REVIEW
-                updatedTask = { ...task, completed: false };
-                setInReview([updatedTask, ...inReview]);
-                break;
-            case "4":  // BACKLOG
-                updatedTask = { ...task, completed: false };
-                setBacklog([updatedTask, ...backlog]);
-                break;
-        }
+  }
+  function setNewState(destinationDroppableId, task) {
+    let updatedTask;
+    switch (destinationDroppableId) {
+      case '1': // TO DO
+        updatedTask = {...task, completed: false};
+        setIncomplete([updatedTask, ...incomplete]);
+        break;
+      case '2': // DONE
+        updatedTask = {...task, completed: true};
+        setCompleted([updatedTask, ...completed]);
+        break;
+      case '3': // IN REVIEW
+        updatedTask = {...task, completed: false};
+        setInReview([updatedTask, ...inReview]);
+        break;
     }
-    function findItemById(id, array) {
-        return array.find((item) => item.id == id);
-    }
+  }
+  function findItemById(id, array) {
+    return array.find((item) => item.id == id);
+  }
 
-    function removeItemById(id, array) {
-        return array.filter((item) => item.id != id);
-    }
+  function removeItemById(id, array) {
+    return array.filter((item) => item.id != id);
+  }
 
-    return (
-        <DragDropContext onDragEnd={handleDragEnd}>
-            <h2 style={{ textAlign: "center" }}>PROGRESS BOARD</h2>
+  return (
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <h2 style={{textAlign: 'center'}}>PROGRESS BOARD</h2>
 
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    flexDirection: "row",
-                    width: "1300px",
-                    margin: "0 auto"
-                }}
-            >
-                <Column title={"TO DO"} tasks={incomplete} id={"1"} />
-                <Column title={"DONE"} tasks={completed} id={"2"} />
-                <Column title={"IN REVIEW"} tasks={inReview} id={"3"} />
-                <Column title={"BACKLOG"} tasks={backlog} id={"4"} />
-            </div>
-        </DragDropContext>
-    );
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          flexDirection: 'row',
+          width: '1300px',
+          margin: '0 auto',
+        }}
+      >
+        <Column title={'To Do'} tasks={incomplete} id={'1'} />
+        <Column title={'In Progress'} tasks={completed} id={'2'} />
+        <Column title={'Done'} tasks={inReview} id={'3'} />
+      </div>
+    </DragDropContext>
+  );
 }
