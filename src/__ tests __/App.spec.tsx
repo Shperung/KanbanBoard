d5fd395883607bb3197de9423395908f436c28a3
Viktor Kravchuk <viktorkrav4uk87@gmail.com>
2024-07-31 20:06:45 +0300

    Update task provider

---
+++ src/__ tests __/App.spec.tsx
@@ -0,0 +1,13 @@
+// src/__ tests __/App.test.tsx
+
+// import {sum} from '../helpers/mergeColumnsData';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+// describe('sum module', () => {
+//   test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+//   });
+// });
--- src/helpers/mergeColumnsData.ts
+++ src/helpers/mergeColumnsData.ts
@@ -1,16 +1,44 @@
-interface Column {
-  id: string;
-  title: string;
-  taskIds: string[];
+import {ColumnsType} from '../providers/taskaTypes';
+import {INIT_COLUMNS} from '../providers/taskPoviderConst';
+
+export function sum(a, b) {
+  return a + b;
 }
 
-interface ColumnsData {
-  [key: string]: Column;
-}
+export const mergeColumnData = (
+  localData: ColumnsType,
+  serverData: ColumnsType
+): ColumnsType => {
+  // Set to keep track of unique taskIds across all columns
+  const uniqueTaskIds = new Set<string>();
 
-export const mergeColumnsData = (
-  localData: ColumnsData,
-  serverData: ColumnsData
-): ColumnsData => {
-  return serverData;
+  // Helper function to merge taskIds and ensure uniqueness within the column and across all columns
+  const mergeTaskIds = (localIds: string[], serverIds: string[]): string[] => {
+    const mergedIds = new Set<string>(localIds);
+
+    serverIds.forEach((id) => {
+      if (!uniqueTaskIds.has(id)) {
+        mergedIds.add(id);
+      }
+    });
+
+    mergedIds.forEach((id) => uniqueTaskIds.add(id));
+
+    return Array.from(mergedIds);
+  };
+
+  // Initialize merged columns with the structure of INIT_COLUMNS
+  const mergedColumnsData: ColumnsType = {...INIT_COLUMNS};
+
+  for (const key in localData) {
+    if (localData.hasOwnProperty(key) && serverData.hasOwnProperty(key)) {
+      mergedColumnsData[key] = {
+        id: localData[key].id,
+        title: localData[key].title,
+        taskIds: mergeTaskIds(localData[key].taskIds, serverData[key].taskIds),
+      };
+    }
+  }
+
+  return mergedColumnsData;
 };
